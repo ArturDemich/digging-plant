408c14849c63743c423312403f869d3e5e1eba8b
ArturDemich <strelokar@gmail.com>
2022-10-07 01:33:29 +0300

    change repo name

--- state/dataService.js
+++ state/dataService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export class DataService {
     static getData() {
 
-        return axios.get('https://my-json-server.typicode.com/ArturDemich/digging-plant/orders')
+        return axios.get('https://my-json-server.typicode.com/ArturDemich/digging-plant/orders') // test endpoint
     }
 
 } 
